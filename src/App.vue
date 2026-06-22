03777d11571ef7d42a60334ab04ac339fb1472f3
kelei321 <51449002+kelei321@users.noreply.github.com>
2026-06-22 13:13:29 +0800

    fix: address template review issues

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,3 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router'
-</script>
-
 <template>
-  <RouterView v-slot="{ Component, route }">
-    <KeepAlive>
-      <component :is="Component" v-if="route.meta.keepAlive" :key="route.fullPath" />
-    </KeepAlive>
-    <component :is="Component" v-if="!route.meta.keepAlive" :key="route.fullPath" />
-  </RouterView>
+  <RouterView />
 </template>
